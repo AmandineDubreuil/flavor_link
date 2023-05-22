65869fe8dfcfd904fe02c2838c3a52759a79e960
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-05-22 11:48:03 +0200

    ajout entité ingredients

---
+++ src/Entity/Ingredients.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientsRepository::class)]
+class Ingredients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $groupe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sousGroupe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ingredient = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGroupe(): ?string
+    {
+        return $this->groupe;
+    }
+
+    public function setGroupe(string $groupe): self
+    {
+        $this->groupe = $groupe;
+
+        return $this;
+    }
+
+    public function getSousGroupe(): ?string
+    {
+        return $this->sousGroupe;
+    }
+
+    public function setSousGroupe(string $sousGroupe): self
+    {
+        $this->sousGroupe = $sousGroupe;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?string
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(string $ingredient): self
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+}

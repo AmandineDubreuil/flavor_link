3afc415ef09c1addd5aef43ea53b95b465924ebc
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-06-12 16:35:20 +0200

    mise en forme index repas

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -31,8 +38,62 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/oubli-pass', name: 'app_forgotten_password')]
-    public function forgottenPassword(): void
+    public function forgottenPassword(
+        Request $request,
+        UserRepository $usersRepository,
+        TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
+    ): Response
     {
-        //return $this->render('security/reset_password_request.html.twig');
+
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //On va chercher l'utilisateur par son email
+            $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+
+            // On vérifie si on a un utilisateur
+            if($user){
+                // On génère un token de réinitialisation
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                // On génère un lien de réinitialisation du mot de passe
+                $url = $this->generateUrl('reset_pass', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+                
+                // On crée les données du mail
+                $context = compact('url', 'user');
+
+                // préparation du mail
+                $email = (new Email())
+                ->from('amandine.dubreuil76@gmail.com')
+                ->to('you@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+
+                // envoi du mail
+                $mailer->send($email);
+
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+            // $user est null
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
     }
 }
